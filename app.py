4e9be50a235dee3711ab2c187f0f2db859e045ea
Dambar Sing <programmer.gshi18@gmail.com>
2026-02-24 22:52:41 +0545

    var, fun and slicing

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-2 +
+# variable
+course = "Python Programming"
+
+# function
+print(len(course))
+print(course[-1])
+
+# slicing
+print(course[0:6])  # Python
+print(course[0:])  # Python Programming
+print(course[:3])  # Pyt
+print(course[:])  # Python Programming
+
+# problem
+course2 = "Python \"Programming"  # Using escaped quotes to include double quotes in the string
+print(course2)
