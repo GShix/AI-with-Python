cb9d004b5021bb47af880658a7c497b03cbfde1d
Dambar Sing <programmer.gshi18@gmail.com>
2026-02-26 21:35:13 +0545

    loops and nested loop

--- app.py
+++ app.py
@@ -130,3 +130,29 @@ age = 12
 # if age >= 18 and age <65:
 # if 18 <= age < 65:
 #     print("Eligible")
+
+# foor loops
+# for number in range(3):
+#     print("Attempt", number+1)
+#     print("Attempt", number+1, (number + 1) * ".")
+
+# for number in range(1, 4): #start from number 1 to 3
+#     print("Attempt", number, number * ".")
+
+# for number in range(1, 10, 2):
+#     print("Attempt", number, number*".")
+
+# for else loop - else block will be executed if the loop is not terminated by a break statement
+successful = True
+# for number in range(3):
+#     print("Attempt", number+1)
+#     if successful:
+#         print("Successful")
+#         break
+# else:
+#     print("Attempted 3 times and failed")
+
+# Nested loops
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
