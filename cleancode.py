82b79aff2f46af42ad2aa9750cbd767c9c90eebc
Dambar Sing <programmer.gshi18@gmail.com>
2026-03-03 22:33:47 +0545

    cleancode

---
+++ cleancode.py
@@ -0,0 +1,10 @@
+def find_area(length, width):
+    """Calculate the area of a rectangle."""
+    return length * width
+
+
+find_area(5, 10)
+
+square_numbers = [i**3 for i in range(10)]
+
+print(square_numbers)
